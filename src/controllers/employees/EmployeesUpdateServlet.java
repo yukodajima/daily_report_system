cb397ec0084724b8e41bf96f1f48e77c15ecadf7
yu.kodajima <koda12131107@gmail.com>
2021-03-31 16:23:18 +0900

    Add Employee destroy

--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -33,19 +33,21 @@ public class EmployeesUpdateServlet extends HttpServlet {
     }
 
     /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())) {
+        if (_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            Employee e = em.find(Employee.class, (Integer)(request.getSession().getAttribute("employee_id")));
+            Employee e = em.find(Employee.class, (Integer) (request.getSession().getAttribute("employee_id")));
 
             // 現在の値と異なる社員番号が入力されていたら
             // 重複チェックを行う指定をする
             Boolean codeDuplicateCheckFlag = true;
-            if(e.getCode().equals(request.getParameter("code"))) {
+            if (e.getCode().equals(request.getParameter("code"))) {
                 codeDuplicateCheckFlag = false;
             } else {
                 e.setCode(request.getParameter("code"));
@@ -55,15 +57,11 @@ public class EmployeesUpdateServlet extends HttpServlet {
             // パスワードの入力値チェックを行う指定をする
             Boolean passwordCheckFlag = true;
             String password = request.getParameter("password");
-            if(password == null || password.equals("")) {
+            if (password == null || password.equals("")) {
                 passwordCheckFlag = false;
             } else {
-                e.setPassword(
-                        EncryptUtil.getPasswordEncrypt(
-                                password,
-                                (String)this.getServletContext().getAttribute("pepper")
-                                )
-                        );
+                e.setPassword(EncryptUtil.getPasswordEncrypt(password,
+                        (String) this.getServletContext().getAttribute("pepper")));
             }
 
             e.setName(request.getParameter("name"));
@@ -72,7 +70,7 @@ public class EmployeesUpdateServlet extends HttpServlet {
             e.setDelete_flag(0);
 
             List<String> errors = EmployeeValidator.validate(e, codeDuplicateCheckFlag, passwordCheckFlag);
-            if(errors.size() > 0) {
+            if (errors.size() > 0) {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
@@ -91,6 +89,6 @@ public class EmployeesUpdateServlet extends HttpServlet {
 
                 response.sendRedirect(request.getContextPath() + "/employees/index");
             }
+        }
     }
-
 }
