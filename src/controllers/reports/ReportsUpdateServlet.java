06ece4ce8fc930575bb58f6c16f5910668211c77
yu.kodajima <koda12131107@gmail.com>
2021-04-08 19:35:03 +0900

    Add Report update

--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -45,6 +45,7 @@ public class ReportsUpdateServlet extends HttpServlet {
             r.setReport_date(Date.valueOf(request.getParameter("report_date")));
             r.setTitle(request.getParameter("title"));
             r.setContent(request.getParameter("content"));
+            r.setBusiness(request.getParameter("business"));
             r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
             List<String> errors = ReportValidator.validate(r);
